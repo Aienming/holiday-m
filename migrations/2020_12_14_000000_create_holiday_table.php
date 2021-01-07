058635bbcd113b8d2dbd5f114fa1966bf8da70c0
xuxiaoming <aienming@163.com>
2021-01-07 17:22:06 +0800

    remark字段允许为空

--- migrations/2020_12_14_000000_create_holiday_table.php
+++ migrations/2020_12_14_000000_create_holiday_table.php
@@ -17,7 +17,7 @@ class CreateHolidayTable extends Migration
             Schema::create('holiday', function(Blueprint $table) {
                 $table->id();
                 $table->string('holiday', 100)->comment('节假日名称');
-                $table->string('remark', 255)->comment('备注');
+                $table->string('remark', 255)->nullable()->comment('备注');
                 $table->date('start')->comment('开始时间');
                 $table->date('end')->comment('结束时间');
                 $table->string('lieuDay', 100)->comment('调休日');
